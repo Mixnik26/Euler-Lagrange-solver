5fae7cfbb641e3032fda4b1ecbb4bdab7d72b80e
Mixtli Monroe <nikolai.mixtli@gmail.com>
2025-04-11 17:53:49 +0100

    Brachistochrone example

--- brachistochrone_example.py
+++ brachistochrone_example.py
@@ -6,21 +6,23 @@ import matplotlib.pyplot as plt
 g = 9.81  # gravitational acceleration
 def Lagrangian(q, qdot):
     x, y = q
+    if y==0:
+        y=2e-5
     xdot, ydot = qdot
-    return np.sqrt((ydot**2 + xdot**2)/(2*g*y))
+    return np.sqrt(np.abs((ydot**2 + xdot**2)/(-2*g*y)))
 
 # Boundary conditions
-initial_pos = np.array([0,11])
-final_pos = np.array([1,10.5])
+initial_pos = np.array([0,-5])
+final_pos = np.array([10,-10])
 
 def bc(qa, qb):
     return qa[0] - initial_pos[0], qa[1] - initial_pos[1], qb[0] - final_pos[0], qb[1] - final_pos[1]
 
 # Initialize the problem
-t = np.linspace(0, 1, 100)
-initial_q_array = np.array([np.linspace(0, 1, 100), np.linspace(10.5,10.5,100), np.zeros(100), np.zeros(100)]) # initial guess for the solution
+t = np.linspace(0, 1, 50)
+initial_q_array = np.array([np.linspace(0, 1, 50), np.linspace(final_pos[1],final_pos[1],50), np.zeros(50), np.zeros(50)]) # initial guess for the solution
 
-brachistochrone = EL.EulerLagrange(Lagrangian)
+brachistochrone = EL.EulerLagrange(Lagrangian, dimensionality=2)
 brachistochrone.bvp(t, bc, initial_q_array, verbose=2)
 
 plt.title('Brachistochrone Problem')
